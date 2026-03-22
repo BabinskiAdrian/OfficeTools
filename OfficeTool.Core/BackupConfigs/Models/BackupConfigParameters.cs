85b56ac8f97a49e3912d10fdcad8230b2908010b
Adrian Babiński <adrian.babinski@protonmail.com>
2026-03-22 16:11:27 +0100

    prepared structure for ConfigBackup

---
+++ OfficeTool.Core/BackupConfigs/Models/BackupConfigParameters.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeTool.Core.BackupConfigs.Models
+{
+    internal class BackupConfigParameters
+    {
+    }
+}
---
+++ OfficeTool.Core/BackupConfigs/Services/IScalanceConfigService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeTool.Core.BackupConfigs.Services
+{
+    internal interface IScalanceConfigService
+    {
+    }
+}
